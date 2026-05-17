defb0846dc86c8e69a33abdf329dac491a799c1f
Xinglei Xu <xingleixu@gmail.com>
2026-05-17 11:59:00 +0800

    Untrack _build_jit.bat and ignore local helper scripts

--- _build_jit.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64 >nul 2>&1
-cmake --build build -j 8 > C:\Users\lenovo\Desktop\build.txt 2>&1
-echo BUILD_EXIT=%ERRORLEVEL% >> C:\Users\lenovo\Desktop\build.txt
