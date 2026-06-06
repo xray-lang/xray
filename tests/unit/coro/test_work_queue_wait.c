3e0292b7510e343b197d6c0fbb4cdd068c9efe30
Xinglei Xu <xingleixu@gmail.com>
2026-06-06 16:40:40 +0800

    Keep work queue waiters blocked without workers

--- tests/unit/coro/test_work_queue_wait.c
+++ tests/unit/coro/test_work_queue_wait.c
@@ -10,6 +10,7 @@
 
 #include "../test_framework.h"
 #include "coro/xcoroutine.h"
+#include "coro/xworker.h"
 #include "coro/xwork_queue.h"
 #include "runtime/gc/xsystem_heap.h"
 #include "runtime/xisolate_internal.h"
@@ -81,9 +82,49 @@ TEST(cancel_waiter_unlinks_coroutine_from_work_queue) {
     work_queue_fixture_cleanup(&f);
 }
 
+TEST(close_without_workers_keeps_waiter_blocked) {
+    WorkQueueFixture f;
+    ASSERT_TRUE(work_queue_fixture_init(&f));
+
+    XrRuntime runtime;
+    memset(&runtime, 0, sizeof(runtime));
+    runtime.isolate = &f.isolate_storage;
+    runtime.worker_count = 0;
+    runtime.workers = NULL;
+    f.isolate_storage.vm.runtime = &runtime;
+
+    XrWorkQueue *q = xr_work_queue_new(&f.isolate_storage, 1, 1);
+    ASSERT_NOT_NULL(q);
+
+    XrCoroutine coro;
+    XrCoroExt ext;
+    init_blocked_work_queue_coro(&coro, &ext, &f.isolate_storage, q);
+
+    q->wait_first = &coro;
+    q->wait_last = &coro;
+    atomic_store(&q->waiter_count, 1);
+
+    xr_work_queue_close(q);
+
+    ASSERT_TRUE(xr_work_queue_is_closed(q));
+    ASSERT_EQ_PTR(q->wait_first, &coro);
+    ASSERT_EQ_PTR(q->wait_last, &coro);
+    ASSERT_EQ_INT((int) atomic_load(&q->waiter_count), 1);
+    ASSERT_EQ_INT(atomic_load(&coro.coro_state), XR_CORO_STATE_BLOCKED);
+    ASSERT_FALSE(xr_coro_flags_has(&coro, XR_CORO_FLG_READY));
+    ASSERT_EQ_INT(atomic_load(&ext.wait.work_queue_token.state), XR_WORK_QUEUE_WAIT_REGISTERED);
+
+    q->wait_first = NULL;
+    q->wait_last = NULL;
+    atomic_store(&q->waiter_count, 0);
+    xr_gc_destroy_work_queue(&q->gc, NULL);
+    work_queue_fixture_cleanup(&f);
+}
+
 TEST_MAIN_BEGIN()
 
 RUN_TEST_SUITE("WorkQueue Wait");
 RUN_TEST(cancel_waiter_unlinks_coroutine_from_work_queue);
+RUN_TEST(close_without_workers_keeps_waiter_blocked);
 
 TEST_MAIN_END()
