436d6b9ef9b53ad3f93d57437b8b4189b32e402e
Xinglei Xu <xingleixu@gmail.com>
2026-06-07 22:24:29 +0800

    Generate closure lowering

--- src/ir/xi_emit.c
+++ src/ir/xi_emit.c
@@ -469,7 +469,6 @@ const XiEmitHandler xi_emit_handlers[XI_OP_COUNT] = {
     [XI_TAIL_CALL] = xi_emit_tail_call,
     [XI_CALL_BUILTIN] = xi_emit_call_builtin,
     [XI_EXTRACT] = xi_emit_extract,
-    [XI_CLOSURE_NEW] = xi_emit_closure_new,
     [XI_GET_GLOBAL] = xi_emit_get_global,
     [XI_SET_GLOBAL] = xi_emit_set_global,
     [XI_PRINT] = xi_emit_print,
