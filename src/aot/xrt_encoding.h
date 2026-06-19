bcd58cf23a1299676cc9307655f0ae7611b3fcc7
Xinglei Xu <xingleixu@gmail.com>
2026-06-20 01:04:19 +0800

    Make encoding UTF-16 helpers freestanding for AOT

--- src/aot/xrt_encoding.h
+++ src/aot/xrt_encoding.h
@@ -80,4 +80,105 @@ static inline XrValue xrt_encoding_utf8_byte_length(const char *data, int64_t le
     return XR_FROM_INT(len < 0 ? 0 : len);
 }
 
+static inline int xrt_encoding_utf16_endian_from_value(XrValue value) {
+    return XR_IS_INT(value) && XR_TO_INT(value) == XR_ENCODING_UTF16_BE ? XR_ENCODING_UTF16_BE
+                                                                        : XR_ENCODING_UTF16_LE;
+}
+
+static inline XrValue xrt_encoding_utf16_encode_impl(const char *data, int64_t len, int endian) {
+    if (!data && len != 0)
+        return XR_NULL_VAL;
+    size_t n = len < 0 ? 0 : (size_t) len;
+    size_t out_len = 0;
+    if (!xr_encoding_core_utf16_encoded_len(data, n, &out_len) || out_len > (size_t) INT64_MAX)
+        return XR_NULL_VAL;
+    XrValue result = xrt_array_new_typed_uninit((int64_t) out_len, XR_ELEM_U8);
+    xrt_array_t *arr = (xrt_array_t *) result.ptr;
+    arr->length = (int64_t) out_len;
+    if (!xr_encoding_core_utf16_encode(data, n, (uint8_t *) arr->data, (size_t) arr->capacity,
+                                       endian, &out_len)) {
+        xrt_release(result);
+        return XR_NULL_VAL;
+    }
+    arr->length = (int64_t) out_len;
+    return result;
+}
+
+static inline XrValue xrt_encoding_utf16_encode(const char *data, int64_t len) {
+    return xrt_encoding_utf16_encode_impl(data, len, XR_ENCODING_UTF16_LE);
+}
+
+static inline XrValue xrt_encoding_utf16_encode_endian(const char *data, int64_t len,
+                                                       XrValue endian) {
+    return xrt_encoding_utf16_encode_impl(data, len, xrt_encoding_utf16_endian_from_value(endian));
+}
+
+static inline int xrt_encoding_value_bytes_view(XrValue value, const uint8_t **out_data,
+                                                size_t *out_len) {
+    if (XR_IS_STR(value)) {
+        *out_data = (const uint8_t *) xr_str_data(value);
+        *out_len = (size_t) xr_str_len(value);
+        return 1;
+    }
+    if (XR_IS_ARRAY(value) && value.ptr) {
+        xrt_array_t *arr = (xrt_array_t *) value.ptr;
+        if (arr->elem_type != XR_ELEM_U8)
+            return 0;
+        *out_data = (const uint8_t *) arr->data;
+        *out_len = arr->length < 0 ? 0 : (size_t) arr->length;
+        return 1;
+    }
+    return 0;
+}
+
+static inline XrValue xrt_encoding_utf16_decode_impl(XrValue input, int endian, int endian_explicit,
+                                                     int strip_bom) {
+    const uint8_t *data = NULL;
+    size_t len = 0;
+    if (!xrt_encoding_value_bytes_view(input, &data, &len))
+        return XR_NULL_VAL;
+
+    if (strip_bom && len >= 2) {
+        if (data[0] == 0xFF && data[1] == 0xFE) {
+            if (!endian_explicit)
+                endian = XR_ENCODING_UTF16_LE;
+            data += 2;
+            len -= 2;
+        } else if (data[0] == 0xFE && data[1] == 0xFF) {
+            if (!endian_explicit)
+                endian = XR_ENCODING_UTF16_BE;
+            data += 2;
+            len -= 2;
+        }
+    }
+
+    size_t out_len = 0;
+    if (!xr_encoding_core_utf16_to_utf8_len(data, len, endian, &out_len) ||
+        out_len > (size_t) INT64_MAX)
+        return XR_NULL_VAL;
+    XrValue result = xrt_str_alloc(out_len);
+    if (!xr_encoding_core_utf16_decode(data, len, xr_str_buf(result), out_len, endian, &out_len)) {
+        xrt_release(result);
+        return XR_NULL_VAL;
+    }
+    xr_str_buf(result)[out_len] = '\0';
+    return result;
+}
+
+static inline XrValue xrt_encoding_utf16_decode(XrValue input) {
+    return xrt_encoding_utf16_decode_impl(input, XR_ENCODING_UTF16_LE, 0, 1);
+}
+
+static inline XrValue xrt_encoding_utf16_decode_endian(XrValue input, XrValue endian) {
+    return xrt_encoding_utf16_decode_impl(input, xrt_encoding_utf16_endian_from_value(endian), 1,
+                                          1);
+}
+
+static inline XrValue xrt_encoding_utf16_decode_endian_strip(XrValue input, XrValue endian,
+                                                             XrValue strip_bom) {
+    int strip = !XR_IS_BOOL(strip_bom) || strip_bom.i != 0;
+    return xrt_encoding_utf16_decode_impl(input, xrt_encoding_utf16_endian_from_value(endian), 1,
+                                          strip);
+}
+
 #endif  // XRT_ENCODING_H
--- src/shared/xr_encoding_core.h
+++ src/shared/xr_encoding_core.h
@@ -17,6 +17,8 @@
 
 #define XR_ENCODING_UNICODE_MAX 0x10FFFFu
 #define XR_ENCODING_UNICODE_INVALID 0xFFFDu
+#define XR_ENCODING_UTF16_LE 0
+#define XR_ENCODING_UTF16_BE 1
 
 static inline uint8_t xr_encoding_core_hex_value(unsigned char c) {
     if (c >= '0' && c <= '9')
@@ -194,6 +196,185 @@ static inline size_t xr_encoding_core_utf8_count(const char *str, size_t len) {
         count++;
     }
     return count;
+}
+
+static inline int xr_encoding_core_utf8_encode_size(uint32_t cp) {
+    if (cp <= 0x7F)
+        return 1;
+    if (cp <= 0x7FF)
+        return 2;
+    if (cp >= 0xD800 && cp <= 0xDFFF)
+        return 0;
+    if (cp <= 0xFFFF)
+        return 3;
+    if (cp <= XR_ENCODING_UNICODE_MAX)
+        return 4;
+    return 0;
+}
+
+static inline int xr_encoding_core_utf8_encode(uint32_t cp, char *output) {
+    int needed = xr_encoding_core_utf8_encode_size(cp);
+    if (!output || needed == 0)
+        return 0;
+    if (needed == 1) {
+        output[0] = (char) cp;
+    } else if (needed == 2) {
+        output[0] = (char) (0xC0 | (cp >> 6));
+        output[1] = (char) (0x80 | (cp & 0x3F));
+    } else if (needed == 3) {
+        output[0] = (char) (0xE0 | (cp >> 12));
+        output[1] = (char) (0x80 | ((cp >> 6) & 0x3F));
+        output[2] = (char) (0x80 | (cp & 0x3F));
+    } else {
+        output[0] = (char) (0xF0 | (cp >> 18));
+        output[1] = (char) (0x80 | ((cp >> 12) & 0x3F));
+        output[2] = (char) (0x80 | ((cp >> 6) & 0x3F));
+        output[3] = (char) (0x80 | (cp & 0x3F));
+    }
+    return needed;
+}
+
+static inline bool xr_encoding_core_add_size(size_t *acc, size_t delta) {
+    if (!acc || *acc > SIZE_MAX - delta)
+        return false;
+    *acc += delta;
+    return true;
+}
+
+static inline bool xr_encoding_core_utf16_encoded_len(const char *utf8, size_t utf8_len,
+                                                      size_t *out_len) {
+    if (!utf8)
+        return false;
+    size_t len = 0;
+    size_t pos = 0;
+    while (pos < utf8_len) {
+        uint32_t cp;
+        int char_len = xr_encoding_core_utf8_decode(utf8 + pos, utf8_len - pos, &cp);
+        if (char_len == 0 || cp == XR_ENCODING_UNICODE_INVALID)
+            return false;
+        if (!xr_encoding_core_add_size(&len, cp > 0xFFFF ? 4u : 2u))
+            return false;
+        pos += (size_t) char_len;
+    }
+    if (out_len)
+        *out_len = len;
+    return true;
+}
+
+static inline void xr_encoding_core_utf16_write_unit(uint8_t *output, size_t *pos, uint16_t unit,
+                                                     int endian) {
+    if (endian == XR_ENCODING_UTF16_BE) {
+        output[(*pos)++] = (uint8_t) (unit >> 8);
+        output[(*pos)++] = (uint8_t) (unit & 0xFF);
+    } else {
+        output[(*pos)++] = (uint8_t) (unit & 0xFF);
+        output[(*pos)++] = (uint8_t) (unit >> 8);
+    }
+}
+
+static inline bool xr_encoding_core_utf16_encode(const char *utf8, size_t utf8_len, uint8_t *output,
+                                                 size_t out_cap, int endian, size_t *out_len) {
+    if ((!utf8 && utf8_len != 0) || (!output && out_cap != 0))
+        return false;
+    size_t expected = 0;
+    if (!xr_encoding_core_utf16_encoded_len(utf8, utf8_len, &expected) || expected > out_cap)
+        return false;
+
+    size_t in_pos = 0;
+    size_t out_pos = 0;
+    while (in_pos < utf8_len) {
+        uint32_t cp;
+        int char_len = xr_encoding_core_utf8_decode(utf8 + in_pos, utf8_len - in_pos, &cp);
+        if (char_len == 0 || cp == XR_ENCODING_UNICODE_INVALID)
+            return false;
+        in_pos += (size_t) char_len;
+
+        if (cp <= 0xFFFF) {
+            xr_encoding_core_utf16_write_unit(output, &out_pos, (uint16_t) cp, endian);
+        } else {
+            cp -= 0x10000;
+            xr_encoding_core_utf16_write_unit(output, &out_pos, (uint16_t) (0xD800u + (cp >> 10)),
+                                              endian);
+            xr_encoding_core_utf16_write_unit(output, &out_pos,
+                                              (uint16_t) (0xDC00u + (cp & 0x3FFu)), endian);
+        }
+    }
+
+    if (out_len)
+        *out_len = out_pos;
+    return true;
+}
+
+static inline uint16_t xr_encoding_core_utf16_read_unit(const uint8_t *data, int endian) {
+    if (endian == XR_ENCODING_UTF16_BE)
+        return (uint16_t) (((uint16_t) data[0] << 8) | (uint16_t) data[1]);
+    return (uint16_t) ((uint16_t) data[0] | ((uint16_t) data[1] << 8));
+}
+
+static inline bool xr_encoding_core_utf16_to_utf8_len(const uint8_t *utf16, size_t utf16_len,
+                                                      int endian, size_t *out_len) {
+    if ((!utf16 && utf16_len != 0) || (utf16_len % 2u) != 0)
+        return false;
+    size_t len = 0;
+    size_t pos = 0;
+    while (pos < utf16_len) {
+        uint16_t unit = xr_encoding_core_utf16_read_unit(utf16 + pos, endian);
+        pos += 2u;
+        uint32_t cp;
+        if (unit >= 0xD800 && unit <= 0xDBFF) {
+            if (pos + 2u > utf16_len)
+                return false;
+            uint16_t low = xr_encoding_core_utf16_read_unit(utf16 + pos, endian);
+            if (low < 0xDC00 || low > 0xDFFF)
+                return false;
+            pos += 2u;
+            cp = 0x10000u + (((uint32_t) unit - 0xD800u) << 10) + ((uint32_t) low - 0xDC00u);
+        } else if (unit >= 0xDC00 && unit <= 0xDFFF) {
+            return false;
+        } else {
+            cp = unit;
+        }
+        int needed = xr_encoding_core_utf8_encode_size(cp);
+        if (needed == 0 || !xr_encoding_core_add_size(&len, (size_t) needed))
+            return false;
+    }
+    if (out_len)
+        *out_len = len;
+    return true;
+}
+
+static inline bool xr_encoding_core_utf16_decode(const uint8_t *utf16, size_t utf16_len,
+                                                 char *output, size_t out_cap, int endian,
+                                                 size_t *out_len) {
+    if ((!utf16 && utf16_len != 0) || (!output && out_cap != 0))
+        return false;
+    size_t expected = 0;
+    if (!xr_encoding_core_utf16_to_utf8_len(utf16, utf16_len, endian, &expected) ||
+        expected > out_cap)
+        return false;
+
+    size_t in_pos = 0;
+    size_t out_pos = 0;
+    while (in_pos < utf16_len) {
+        uint16_t unit = xr_encoding_core_utf16_read_unit(utf16 + in_pos, endian);
+        in_pos += 2u;
+        uint32_t cp;
+        if (unit >= 0xD800 && unit <= 0xDBFF) {
+            uint16_t low = xr_encoding_core_utf16_read_unit(utf16 + in_pos, endian);
+            in_pos += 2u;
+            cp = 0x10000u + (((uint32_t) unit - 0xD800u) << 10) + ((uint32_t) low - 0xDC00u);
+        } else {
+            cp = unit;
+        }
+        int written = xr_encoding_core_utf8_encode(cp, output + out_pos);
+        if (written == 0)
+            return false;
+        out_pos += (size_t) written;
+    }
+
+    if (out_len)
+        *out_len = out_pos;
+    return true;
 }
 
 #endif  // XR_ENCODING_CORE_H
