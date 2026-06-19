fe66dd88757f56f23a2e1d45393aeba1fba72e67
Xinglei Xu <xingleixu@gmail.com>
2026-06-20 00:42:52 +0800

    Make base64 bytes helpers freestanding for AOT

--- src/aot/xrt.h
+++ src/aot/xrt.h
@@ -30,10 +30,10 @@
 #include "xrt_path.h"       // L0: freestanding path string helpers
 #include "xrt_encoding.h"   // L0: freestanding encoding predicates/counts
 #include "xrt_arc.h"        // L1: bump alloc, xrt_str_alloc, xrt_str_concat
-#include "xrt_base64.h"     // L1: freestanding Base64 string helpers
 #include "xrt_range.h"      // L1: lazy Range value
 #include "xrt_arith.h"      // L1: add/sub/mul/div/mod/neg, eq/lt/le, print
 #include "xrt_coll.h"       // L2: Array, Map, StringBuilder, Closure, index ops
+#include "xrt_base64.h"     // L2: freestanding Base64 string/Bytes helpers
 #include "xrt_method.h"     // L3: method_0/1/2, getprop, tostring, symbol IDs
 #include "xrt_exception.h"  // L4: setjmp/longjmp exception handling
 #include "xrt_defer.h"      // L4: function-scoped deferred cleanup (uses L1/L2/L4)
--- src/aot/xrt_base64.h
+++ src/aot/xrt_base64.h
@@ -12,7 +12,8 @@
 #define XRT_BASE64_H
 
 #include "../shared/xr_base64_core.h"
-#include "xrt_arc.h"
+#include "xrt_coll.h"
+#include <stdint.h>
 #include <string.h>
 
 static inline XrValue xrt_base64_is_valid(const char *data, int64_t len) {
@@ -20,7 +21,7 @@ static inline XrValue xrt_base64_is_valid(const char *data, int64_t len) {
 }
 
 static inline XrValue xrt_base64_encode_impl(const char *data, int64_t len, bool url_safe) {
-    if (!data)
+    if (!data && len != 0)
         return XR_NULL_VAL;
     size_t n = len < 0 ? 0 : (size_t) len;
     size_t out_len = 0;
@@ -56,6 +57,54 @@ static inline XrValue xrt_base64_decode(const char *data, int64_t len) {
 
 static inline XrValue xrt_base64_decode_url(const char *data, int64_t len) {
     return xrt_base64_decode(data, len);
+}
+
+static inline XrValue xrt_base64_encode_bytes(XrValue bytes) {
+    if (!XR_IS_ARRAY(bytes) || !bytes.ptr)
+        return XR_NULL_VAL;
+
+    xrt_array_t *arr = (xrt_array_t *) bytes.ptr;
+    if (arr->length <= 0)
+        return xrt_base64_encode_impl(NULL, 0, false);
+
+    size_t len = (size_t) arr->length;
+    if (arr->elem_type == XR_ELEM_U8)
+        return xrt_base64_encode_impl((const char *) arr->data, (int64_t) len, false);
+
+    if (arr->length > INT32_MAX)
+        return XR_NULL_VAL;
+    unsigned char *tmp = (unsigned char *) XRT_MALLOC(len);
+    if (!tmp)
+        return XR_NULL_VAL;
+    for (int64_t i = 0; i < arr->length; i++) {
+        XrValue item = xr_typed_get(arr->data, (int32_t) i, arr->elem_type);
+        tmp[i] = (unsigned char) xr_value_to_int64_coerce(item);
+    }
+    XrValue result = xrt_base64_encode_impl((const char *) tmp, (int64_t) len, false);
+    XRT_FREE(tmp);
+    return result;
+}
+
+static inline XrValue xrt_base64_decode_to_bytes(const char *data, int64_t len) {
+    if (!data)
+        return XR_NULL_VAL;
+    size_t n = len < 0 ? 0 : (size_t) len;
+    if (!xr_base64_core_is_valid(data, n))
+        return XR_NULL_VAL;
+
+    size_t out_len = 0;
+    if (!xr_base64_core_decoded_len(data, n, &out_len) || out_len > (size_t) INT64_MAX)
+        return XR_NULL_VAL;
+
+    XrValue result = xrt_array_new_typed_uninit((int64_t) out_len, XR_ELEM_U8);
+    xrt_array_t *arr = (xrt_array_t *) result.ptr;
+    arr->length = (int64_t) out_len;
+    if (!xr_base64_core_decode(data, n, (unsigned char *) arr->data, &out_len)) {
+        xrt_release(result);
+        return XR_NULL_VAL;
+    }
+    arr->length = (int64_t) out_len;
+    return result;
 }
 
 #endif  // XRT_BASE64_H
