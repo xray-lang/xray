b4366b1648705e555aac5ba6caf14c966aa21f04
Xinglei Xu <xingleixu@gmail.com>
2026-06-22 08:00:48 +0800

    Split coroutine VM native bindings

--- src/coro/xresult_group.c
+++ src/coro/xresult_group.c
@@ -16,25 +16,11 @@
 #include "../base/xmalloc.h"
 #include "../runtime/core/xr_runtime_core.h"
 #include "../runtime/gc/xsystem_heap.h"
-#include "../runtime/object/xexception.h"
-#include "../runtime/object/xnative_type.h"
-#include "../runtime/object/xtuple.h"
-#include "../runtime/xisolate_api.h"
-#include "../runtime/xisolate_internal.h"
 #include "../runtime/xshared.h"
-#include "../vm/xvm.h"
 #include "xblock.h"
 #include "xcoroutine.h"
 #include "xworker.h"
 #include "xyieldable.h"
-
-static uint32_t sanitize_batch_size(int64_t value) {
-    if (value <= 0)
-        return XR_RESULT_GROUP_DEFAULT_BATCH;
-    if (value > XR_RESULT_GROUP_MAX_BATCH)
-        return XR_RESULT_GROUP_MAX_BATCH;
-    return (uint32_t) value;
-}
 
 static XrRuntime *result_group_runtime(XrResultGroup *g) {
     return g ? (XrRuntime *) g->scheduler : NULL;
@@ -482,40 +468,6 @@ void xr_gc_destroy_result_group(XrGCHeader *obj, XrCoroGC *owning_gc) {
     g->batch_last = NULL;
 }
 
-static XrValue m_add(XrayIsolate *isolate, XrValue self, XrValue *args, int nargs) {
-    (void) isolate;
-    (void) nargs;
-    XrResultGroup *g = xr_value_to_result_group(self);
-    XR_DCHECK(g != NULL, "ResultGroup.add: NULL group");
-    XR_DCHECK(nargs >= 1, "ResultGroup.add: missing value");
-    if (!XR_IS_INT(args[0]))
-        return xr_bool(false);
-    return xr_bool(xr_result_group_add(g, XR_TO_INT(args[0])));
-}
-
-static XrValue m_flush(XrayIsolate *isolate, XrValue self, XrValue *args, int nargs) {
-    (void) isolate;
-    (void) args;
-    (void) nargs;
-    xr_result_group_flush(xr_value_to_result_group(self));
-    return xr_null();
-}
-
-static XrValue m_try_recv(XrayIsolate *isolate, XrValue self, XrValue *args, int nargs) {
-    (void) args;
-    (void) nargs;
-    XrResultGroup *g = xr_value_to_result_group(self);
-    XR_DCHECK(g != NULL, "ResultGroup.tryRecv: NULL group");
-    int64_t value = 0;
-    bool ok = xr_result_group_try_recv(g, &value);
-    XrTuple *tuple = xr_tuple_new(xr_current_coro(isolate), 2);
-    if (!tuple)
-        return xr_null();
-    xr_tuple_set(tuple, 0, ok ? xr_int(value) : xr_null());
-    xr_tuple_set(tuple, 1, xr_bool(ok));
-    return xr_value_from_tuple(tuple);
-}
-
 static void result_group_finish_wait_if_current(XrCoroutine *coro, XrResultGroup *g) {
     XrCoroWaitState *wait = xr_coro_wait_state(coro);
     if (!wait)
@@ -588,111 +540,6 @@ XrResultGroupRecvStatus xr_result_group_recv_resume_for_coro(XrCoroutine *coro, 
     if (!g)
         return XR_RESULT_GROUP_RECV_ERROR;
     return xr_result_group_recv_for_coro(g, coro, result);
-}
-
-static XrCFuncResult ym_recv(XrayIsolate *isolate, XrValue self, XrValue *args, int nargs,
-                             XrValue *result) {
-    (void) args;
-    (void) nargs;
-    XrResultGroup *g = xr_value_to_result_group(self);
-    XR_DCHECK(g != NULL, "ResultGroup.recv: NULL group");
-    XR_DCHECK(result != NULL, "ResultGroup.recv: NULL result");
-
-    switch (xr_result_group_recv_for_coro(g, xr_current_coro(isolate), result)) {
-        case XR_RESULT_GROUP_RECV_DONE:
-            return XR_CFUNC_DONE;
-        case XR_RESULT_GROUP_RECV_BLOCKED:
-            return XR_CFUNC_BLOCKED;
-        case XR_RESULT_GROUP_RECV_ERROR:
-        default:
-            return XR_CFUNC_ERROR;
-    }
-}
-
-static XrValue m_close(XrayIsolate *isolate, XrValue self, XrValue *args, int nargs) {
-    (void) isolate;
-    (void) args;
-    (void) nargs;
-    xr_result_group_close(xr_value_to_result_group(self));
-    return xr_null();
-}
-
-static XrValue g_length(XrayIsolate *isolate, XrValue self, XrValue *args, int nargs) {
-    (void) isolate;
-    (void) args;
-    (void) nargs;
-    return xr_int((int64_t) xr_result_group_length(xr_value_to_result_group(self)));
-}
-
-static XrValue g_pending_count(XrayIsolate *isolate, XrValue self, XrValue *args, int nargs) {
-    (void) isolate;
-    (void) args;
-    (void) nargs;
-    return xr_int((int64_t) xr_result_group_pending_count(xr_value_to_result_group(self)));
-}
-
-static XrValue g_batch_size(XrayIsolate *isolate, XrValue self, XrValue *args, int nargs) {
-    (void) isolate;
-    (void) args;
-    (void) nargs;
-    XrResultGroup *g = xr_value_to_result_group(self);
-    return xr_int(g ? (int64_t) g->batch_size : 0);
-}
-
-static XrValue g_is_closed(XrayIsolate *isolate, XrValue self, XrValue *args, int nargs) {
-    (void) isolate;
-    (void) args;
-    (void) nargs;
-    return xr_bool(xr_result_group_is_closed(xr_value_to_result_group(self)));
-}
-
-static XrValue result_group_construct(XrayIsolate *isolate, XrValue receiver, XrValue *args,
-                                      int nargs) {
-    (void) receiver;
-    uint32_t batch_size = XR_RESULT_GROUP_DEFAULT_BATCH;
-    if (nargs >= 1 && XR_IS_INT(args[0]))
-        batch_size = sanitize_batch_size(XR_TO_INT(args[0]));
-
-    XrResultGroup *g = xr_result_group_new(xr_isolate_get_runtime_core(isolate),
-                                           xr_isolate_get_scheduler_runtime(isolate), batch_size);
-    if (!g) {
-        XrValue exc =
-            xr_exception_newf(isolate, XR_ERR_OUT_OF_MEMORY, "ResultGroup allocation failed");
-        xr_vm_throw_exception(isolate, exc);
-        return xr_null();
-    }
-    return xr_value_from_result_group(g);
-}
-
-void xr_result_group_register_native_type(XrayIsolate *isolate) {
-    static const XrNativeMethod result_group_methods[] = {
-        {"add", m_add, 1},     {"flush", m_flush, 0}, {"tryRecv", m_try_recv, 0},
-        {"close", m_close, 0}, {NULL, NULL, 0},
-    };
-    static const XrNativeYieldableMethod result_group_yieldable_methods[] = {
-        {"recv", ym_recv, 0},
-        {NULL, NULL, 0},
-    };
-    static const XrNativeMethod result_group_getters[] = {
-        {"length", g_length, 0},
-        {"pendingCount", g_pending_count, 0},
-        {"batchSize", g_batch_size, 0},
-        {"isClosed", g_is_closed, 0},
-        {NULL, NULL, 0},
-    };
-    static const XrNativeMethod result_group_statics[] = {
-        {"call", result_group_construct, 0},
-        {NULL, NULL, 0},
-    };
-    static const XrNativeTypeInfo info = {
-        .name = "ResultGroup",
-        .gc_type = XR_TRESULTGROUP,
-        .methods = (XrNativeMethod *) result_group_methods,
-        .yieldable_methods = (XrNativeYieldableMethod *) result_group_yieldable_methods,
-        .getters = (XrNativeMethod *) result_group_getters,
-        .static_methods = (XrNativeMethod *) result_group_statics,
-    };
-    xr_register_native_type(isolate, &info);
 }
 
 #undef RESULT_GROUP_METRIC_ADD
