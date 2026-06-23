d78a2b616b62db84655a5735b9b97ea4971c44c1
Xinglei Xu <xingleixu@gmail.com>
2026-06-23 17:58:41 +0800

    Add AOT DateTime light object

--- src/aot/xrt_value.h
+++ src/aot/xrt_value.h
@@ -135,6 +135,7 @@ typedef struct XrValue {
 #define XR_TAG_ENUM 24     /* AOT bridged enum key */
 #define XR_TAG_ITERATOR 25 /* AOT map/set iterator (for-in over the iterator protocol) */
 #define XR_TAG_REGEX 26    /* AOT compiled regex handle */
+#define XR_TAG_DATETIME 27 /* AOT DateTime light object */
 
 typedef struct XrAotEnumValueView {
     uint64_t gc_words[2];
