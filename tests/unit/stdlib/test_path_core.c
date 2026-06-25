bc041f196eea663cfb51704f96ca96246dc2b62e
Xinglei Xu <xingleixu@gmail.com>
2026-06-26 06:07:02 +0800

    Share path parse field schema

--- tests/unit/stdlib/test_path_core.c
+++ tests/unit/stdlib/test_path_core.c
@@ -45,6 +45,48 @@ TEST(path_core_constants_match_target) {
 #endif
 }
 
+static void assert_slice_eq(XrPathCoreSlice slice, const char *expected) {
+    size_t expected_len = expected ? strlen(expected) : 0;
+    ASSERT_EQ_UINT(slice.len, expected_len);
+    if (expected_len > 0) {
+        ASSERT_NOT_NULL(slice.data);
+        ASSERT_EQ_INT(memcmp(slice.data, expected, expected_len), 0);
+    }
+}
+
+TEST(path_core_parse_schema_matches_pathinfo_order) {
+    ASSERT_EQ_INT(XR_PATH_CORE_PARSE_FIELD_COUNT, 5);
+    ASSERT_STR_EQ(xr_path_core_parse_field_name(XR_PATH_CORE_PARSE_FIELD_ROOT), "root");
+    ASSERT_STR_EQ(xr_path_core_parse_field_name(XR_PATH_CORE_PARSE_FIELD_DIR), "dir");
+    ASSERT_STR_EQ(xr_path_core_parse_field_name(XR_PATH_CORE_PARSE_FIELD_BASE), "base");
+    ASSERT_STR_EQ(xr_path_core_parse_field_name(XR_PATH_CORE_PARSE_FIELD_NAME), "name");
+    ASSERT_STR_EQ(xr_path_core_parse_field_name(XR_PATH_CORE_PARSE_FIELD_EXT), "ext");
+    ASSERT_NULL(xr_path_core_parse_field_name(XR_PATH_CORE_PARSE_FIELD_COUNT));
+
+    const char *const *names = xr_path_core_parse_field_names();
+    ASSERT_STR_EQ(names[0], "root");
+    ASSERT_STR_EQ(names[1], "dir");
+    ASSERT_STR_EQ(names[2], "base");
+    ASSERT_STR_EQ(names[3], "name");
+    ASSERT_STR_EQ(names[4], "ext");
+}
+
+TEST(path_core_parse_plan_field_accessor_follows_schema) {
+    const char *path = "/home/user/file.txt";
+    XrPathCoreParsePlan plan;
+    ASSERT(xr_path_core_parse_plan(path, strlen(path), &plan));
+
+    assert_slice_eq(xr_path_core_parse_plan_field(&plan, XR_PATH_CORE_PARSE_FIELD_ROOT), "/");
+    assert_slice_eq(xr_path_core_parse_plan_field(&plan, XR_PATH_CORE_PARSE_FIELD_DIR),
+                    "/home/user");
+    assert_slice_eq(xr_path_core_parse_plan_field(&plan, XR_PATH_CORE_PARSE_FIELD_BASE),
+                    "file.txt");
+    assert_slice_eq(xr_path_core_parse_plan_field(&plan, XR_PATH_CORE_PARSE_FIELD_NAME), "file");
+    assert_slice_eq(xr_path_core_parse_plan_field(&plan, XR_PATH_CORE_PARSE_FIELD_EXT), ".txt");
+    assert_slice_eq(xr_path_core_parse_plan_field(&plan, XR_PATH_CORE_PARSE_FIELD_COUNT), "");
+    assert_slice_eq(xr_path_core_parse_plan_field(NULL, XR_PATH_CORE_PARSE_FIELD_ROOT), "");
+}
+
 static void assert_format(const char *dir, const char *base, const char *name, const char *ext,
                           const char *expected) {
     XrPathCoreFormatPlan plan;
@@ -148,6 +190,8 @@ TEST(path_core_resolve_fallback_cwd_is_root) {
 TEST_MAIN_BEGIN()
 RUN_TEST_SUITE("path core");
 RUN_TEST(path_core_constants_match_target);
+RUN_TEST(path_core_parse_schema_matches_pathinfo_order);
+RUN_TEST(path_core_parse_plan_field_accessor_follows_schema);
 RUN_TEST(path_core_format_joins_dir_and_base_once);
 RUN_TEST(path_core_format_derives_base_from_name_ext);
 RUN_TEST(path_core_format_base_wins_over_name_ext);
