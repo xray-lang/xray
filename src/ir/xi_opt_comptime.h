566392327730c1e4617f3404b1c353365d7d0c30
Xinglei Xu <xingleixu@gmail.com>
2026-07-06 17:10:36 +0800

    refactor: rename IR const fixpoint pass

--- src/ir/xi_opt_comptime.h
+++ src/ir/xi_opt_comptime.h
@@ -5,7 +5,7 @@
  * Copyright (c) 2026 Xinglei Xu <xingleixu@gmail.com>
  * Licensed under the MIT License
  *
- * xi_opt_comptime.h - Fixpoint driver for compile-time constant simplification
+ * xi_opt_comptime.h - Fixpoint driver for IR constant simplification
  */
 
 #ifndef XI_OPT_COMPTIME_H
@@ -13,10 +13,10 @@
 
 #include "xi_pass.h"
 
-#define XI_COMPTIME_MAX_ROUNDS 8
+#define XI_CONST_FIXPOINT_MAX_ROUNDS 8
 
 /* Run const-folding/copy-prop/SCCP/DCE pipeline to a fixed point.
  * This is an IR optimization pass, not the frontend `comptime` evaluator. */
-XR_FUNC XiPassChange xi_opt_comptime_eval(XiFunc *f);
+XR_FUNC XiPassChange xi_opt_const_fixpoint(XiFunc *f);
 
 #endif /* XI_OPT_COMPTIME_H */
