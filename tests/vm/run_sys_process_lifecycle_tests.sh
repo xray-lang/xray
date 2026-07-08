5d903f7d5401ff73fc7fc3f703ed024d1bb270bd
Xinglei Xu <xingleixu@gmail.com>
2026-07-08 22:39:49 +0800

    feat(analysis): track assignment lifecycle aliases

--- tests/vm/run_sys_process_lifecycle_tests.sh
+++ tests/vm/run_sys_process_lifecycle_tests.sh
@@ -76,6 +76,7 @@ PROCESS_TRYWAIT_ONCE_WARNING_SRC="$PROJECT_DIR/tests/vm/sys_process_lifecycle_tr
 CONTROL_FLOW_OK_SRC="$PROJECT_DIR/tests/vm/sys_process_lifecycle_control_flow_ok.xr"
 EXPR_CONTAINERS_OK_SRC="$PROJECT_DIR/tests/vm/sys_process_lifecycle_expr_containers_ok.xr"
 DESTRUCTURE_ALIAS_OK_SRC="$PROJECT_DIR/tests/vm/sys_process_lifecycle_destructure_alias_ok.xr"
+ASSIGNMENT_ALIAS_OK_SRC="$PROJECT_DIR/tests/vm/sys_process_lifecycle_assignment_alias_ok.xr"
 PROCESS_TRY_CATCH_WARNING_SRC="$PROJECT_DIR/tests/vm/sys_process_lifecycle_try_catch_warning.xr"
 PIPE_MATCH_WARNING_SRC="$PROJECT_DIR/tests/vm/sys_pipe_lifecycle_match_warning.xr"
 
@@ -84,6 +85,7 @@ expect_output "process_pipe_lifecycle_control_flow_ok" "$CONTROL_FLOW_OK_SRC" $'
 expect_output "process_pipe_lifecycle_expr_containers_ok" "$EXPR_CONTAINERS_OK_SRC" \
     $'0\ntrue\ncode 0\n2\n0\ntrue\ntrue'
 expect_output "process_pipe_lifecycle_destructure_alias_ok" "$DESTRUCTURE_ALIAS_OK_SRC" $'0\ntrue'
+expect_output "process_pipe_lifecycle_assignment_alias_ok" "$ASSIGNMENT_ALIAS_OK_SRC" $'0\ntrue'
 expect_warning "process_orphan" "$PROCESS_ORPHAN_SRC" "process-orphan" \
     "sys.Process.spawn returns a Process handle; call wait() explicitly"
 expect_warning "pipe_orphan" "$PIPE_ORPHAN_SRC" "pipe-orphan" \
