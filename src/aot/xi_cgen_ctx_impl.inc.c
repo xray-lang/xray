9ea0cfe51d9a1c4237c8372df2c1b893b123daab
Xinglei Xu <xingleixu@gmail.com>
2026-07-08 22:52:54 +0800

    fix(aot): retain generated no_alloc stdlib details

--- src/aot/xi_cgen_ctx_impl.inc.c
+++ src/aot/xi_cgen_ctx_impl.inc.c
@@ -64,6 +64,9 @@ XR_FUNC void xi_cgen_ctx_free(XiCgenCtx *ctx) {
     xr_free(ctx->phi_repr);
     xr_free(ctx->array_data_cache_decls);
     xr_free(ctx->no_alloc_summaries);
+    for (int i = 0; i < ctx->nno_alloc_detail_strings; i++)
+        xr_free(ctx->no_alloc_detail_strings[i]);
+    xr_free(ctx->no_alloc_detail_strings);
     xr_free(ctx);
 }
 
