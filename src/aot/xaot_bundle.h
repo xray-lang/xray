de0fd7a469720daaf5e0af11aa65347d8e956c1b
Xinglei Xu <xingleixu@gmail.com>
2026-07-09 02:31:40 +0800

    aot: include method identity in dispatch targets

--- src/aot/xaot_bundle.h
+++ src/aot/xaot_bundle.h
@@ -454,6 +454,8 @@ typedef struct XaotDispatchTargetCase {
     XgCallsiteId callsite_id;
     XgClassId receiver_class_id;
     XgMethodId method_id;
+    uint32_t method_name_id;
+    uint32_t method_signature_key;
     XgMethodId method_root_id;
     uint32_t method_override_depth;
     uint32_t evidence;
