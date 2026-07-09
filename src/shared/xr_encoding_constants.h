e00e42ad55bcef8f2c1618bc84250a1f0ef0c172
Xinglei Xu <xingleixu@gmail.com>
2026-07-09 17:59:07 +0800

    refactor(encoding): remove dead shared C core

---
+++ src/shared/xr_encoding_constants.h
@@ -0,0 +1,17 @@
+/*
+ * xray - Lightweight typed scripting with native concurrency
+ * https://www.xray-lang.org
+ *
+ * Copyright (c) 2026 Xinglei Xu <xingleixu@gmail.com>
+ * Licensed under the MIT License
+ *
+ * xr_encoding_constants.h - Shared encoding constants used by lowering.
+ */
+
+#ifndef XR_ENCODING_CONSTANTS_H
+#define XR_ENCODING_CONSTANTS_H
+
+#define XR_ENCODING_UTF16_LE 0
+#define XR_ENCODING_UTF16_BE 1
+
+#endif  // XR_ENCODING_CONSTANTS_H
