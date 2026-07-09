654f20aacbfeb3f56a8b1fda703b186347d8b89d
Xinglei Xu <xingleixu@gmail.com>
2026-07-09 12:48:19 +0800

    fix(sys): summarize lifecycle helpers to fixpoint

--- tests/vm/run_sys_process_lifecycle_tests.sh
+++ tests/vm/run_sys_process_lifecycle_tests.sh
@@ -115,6 +115,8 @@ PROCESS_HELPER_RETURN_ALIAS_WAIT_SRC="$PROJECT_DIR/tests/vm/sys_process_lifecycl
 PIPE_HELPER_RETURN_ALIAS_CLOSE_SRC="$PROJECT_DIR/tests/vm/sys_pipe_lifecycle_helper_return_alias_close.xr"
 PROCESS_HELPER_CHAINED_RETURN_ALIAS_WAIT_SRC="$PROJECT_DIR/tests/vm/sys_process_lifecycle_helper_chained_return_alias_wait.xr"
 PIPE_HELPER_CHAINED_RETURN_ALIAS_CLOSE_SRC="$PROJECT_DIR/tests/vm/sys_pipe_lifecycle_helper_chained_return_alias_close.xr"
+PROCESS_HELPER_FORWARD_RETURN_ALIAS_WAIT_SRC="$PROJECT_DIR/tests/vm/sys_process_lifecycle_helper_forward_return_alias_wait.xr"
+PIPE_HELPER_FORWARD_RETURN_ALIAS_CLOSE_SRC="$PROJECT_DIR/tests/vm/sys_pipe_lifecycle_helper_forward_return_alias_close.xr"
 PROCESS_HELPER_EARLY_RETURN_WARNING_SRC="$PROJECT_DIR/tests/vm/sys_process_lifecycle_helper_early_return_warning.xr"
 
 expect_output "process_pipe_lifecycle_ok" "$OK_SRC" $'0\ntrue\ntrue\ntrue\ntrue'
@@ -145,6 +147,10 @@ expect_output "process_helper_chained_return_alias_wait" \
     "$PROCESS_HELPER_CHAINED_RETURN_ALIAS_WAIT_SRC" "0"
 expect_output "pipe_helper_chained_return_alias_close" \
     "$PIPE_HELPER_CHAINED_RETURN_ALIAS_CLOSE_SRC" "true"
+expect_output "process_helper_forward_return_alias_wait" \
+    "$PROCESS_HELPER_FORWARD_RETURN_ALIAS_WAIT_SRC" "0"
+expect_output "pipe_helper_forward_return_alias_close" \
+    "$PIPE_HELPER_FORWARD_RETURN_ALIAS_CLOSE_SRC" "true"
 expect_warning "process_orphan" "$PROCESS_ORPHAN_SRC" "process-orphan" \
     "sys.Process.spawn returns a Process handle; call wait() explicitly"
 expect_warning "pipe_orphan" "$PIPE_ORPHAN_SRC" "pipe-orphan" \
