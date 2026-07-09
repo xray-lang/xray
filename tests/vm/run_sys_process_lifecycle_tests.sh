65729c9ab539f305f8eca98a1f0dc52e4cd174b3
Xinglei Xu <xingleixu@gmail.com>
2026-07-09 13:54:52 +0800

    fix(sys): resolve nullish lifecycle aliases

--- tests/vm/run_sys_process_lifecycle_tests.sh
+++ tests/vm/run_sys_process_lifecycle_tests.sh
@@ -116,6 +116,7 @@ PROCESS_HELPER_RETURN_ALIAS_WAIT_SRC="$PROJECT_DIR/tests/vm/sys_process_lifecycl
 PIPE_HELPER_RETURN_ALIAS_CLOSE_SRC="$PROJECT_DIR/tests/vm/sys_pipe_lifecycle_helper_return_alias_close.xr"
 NESTED_HELPER_RETURN_ALIAS_OK_SRC="$PROJECT_DIR/tests/vm/sys_process_lifecycle_nested_helper_return_alias_ok.xr"
 TERNARY_HELPER_RETURN_ALIAS_OK_SRC="$PROJECT_DIR/tests/vm/sys_process_lifecycle_ternary_helper_return_alias_ok.xr"
+NULLISH_HELPER_RETURN_ALIAS_OK_SRC="$PROJECT_DIR/tests/vm/sys_process_lifecycle_nullish_helper_return_alias_ok.xr"
 UNSAFE_HELPER_RETURN_ALIAS_OK_SRC="$PROJECT_DIR/tests/vm/sys_process_lifecycle_unsafe_helper_return_alias_ok.xr"
 UNSAFE_RETURN_ALIAS_OK_SRC="$PROJECT_DIR/tests/vm/sys_process_lifecycle_unsafe_return_alias_ok.xr"
 PROCESS_HELPER_CHAINED_RETURN_ALIAS_WAIT_SRC="$PROJECT_DIR/tests/vm/sys_process_lifecycle_helper_chained_return_alias_wait.xr"
@@ -162,6 +163,8 @@ expect_output "process_pipe_nested_helper_return_alias_ok" \
     "$NESTED_HELPER_RETURN_ALIAS_OK_SRC" $'0\ntrue'
 expect_output "process_pipe_ternary_helper_return_alias_ok" \
     "$TERNARY_HELPER_RETURN_ALIAS_OK_SRC" $'0\ntrue'
+expect_output "process_pipe_nullish_helper_return_alias_ok" \
+    "$NULLISH_HELPER_RETURN_ALIAS_OK_SRC" $'0\ntrue'
 expect_output "process_pipe_unsafe_helper_return_alias_ok" \
     "$UNSAFE_HELPER_RETURN_ALIAS_OK_SRC" $'0\ntrue'
 expect_output "process_pipe_unsafe_return_alias_ok" \
