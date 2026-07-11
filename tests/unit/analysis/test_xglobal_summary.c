1e3753292d1e485ccbeac3e069cc0f60a8da17a5
Xinglei Xu <xingleixu@gmail.com>
2026-07-12 02:36:35 +0800

    test(aot): cover json array closure shapes

--- tests/unit/analysis/test_xglobal_summary.c
+++ tests/unit/analysis/test_xglobal_summary.c
@@ -12341,6 +12341,59 @@ TEST(global_evidence_producer_propagates_json_shape_through_closure_capture) {
     teardown_parser_session();
 }
 
+TEST(global_evidence_producer_propagates_json_shape_through_array_closure_container) {
+    setup_parser_session();
+    const char *source = "fn readCapturedArray() -> int {\n"
+                         "    var users: Array<Json> = []\n"
+                         "    users.push({ name: \"ada\", age: 7 })\n"
+                         "    const read = fn() -> int {\n"
+                         "        return users[0].age\n"
+                         "    }\n"
+                         "    return read()\n"
+                         "}\n";
+    AstNode *ast = xr_parse(g_session, source);
+    ASSERT_NOT_NULL(ast);
+    XrModuleSpec spec;
+    memset(&spec, 0, sizeof(spec));
+    spec.source_path = "test.xr";
+    spec.ast = ast;
+    int topo_order[1] = {0};
+    XrModuleGraph graph;
+    memset(&graph, 0, sizeof(graph));
+    graph.specs = &spec;
+    graph.spec_count = 1;
+    graph.topo_order = topo_order;
+    graph.topo_count = 1;
+    graph.entry_index = 0;
+
+    XgGlobalEvidence ev;
+    ASSERT_TRUE(
+        xg_global_evidence_build_from_module_graph(&ev, &graph, XG_BUILD_NATIVE_RELEASE, 0));
+    const XgBodySummary *outer = evidence_find_body_by_name(&ev, "readCapturedArray");
+    const XgBodySummary *closure = evidence_find_body_by_name(&ev, "<anonymous>");
+    ASSERT_NOT_NULL(outer);
+    ASSERT_NOT_NULL(closure);
+    ASSERT_TRUE(outer->func_id != closure->func_id);
+    ASSERT_TRUE(ev.njson_shapes >= 1);
+    ASSERT_EQ_UINT(ev.njson_accesses, 1);
+    ASSERT_EQ_UINT(ev.json_accesses[0].owner_func_id, closure->func_id);
+    ASSERT_TRUE(ev.json_accesses[0].receiver_shape_id != XG_NO_ID);
+    ASSERT_EQ_UINT(ev.json_accesses[0].access_kind, XG_JSON_ACCESS_FIELD_GET);
+    ASSERT_EQ_UINT(ev.json_accesses[0].field_ordinal, 1);
+    ASSERT_TRUE((ev.json_accesses[0].flags & XG_JSON_ACCESS_RECEIVER_SHAPE_PROVEN) != 0);
+
+    XaotBundle bundle;
+    memset(&bundle, 0, sizeof(bundle));
+    ASSERT_TRUE(xaot_bundle_set_global_evidence(&bundle, &ev, XG_BUILD_NATIVE_RELEASE));
+    ASSERT_EQ_UINT(bundle.njson_access_plans, 1);
+    ASSERT_EQ_UINT(bundle.json_access_plans[0].action, XAOT_JSON_ACCESS_DIRECT_INDEX);
+    ASSERT_EQ_UINT(bundle.json_access_plans[0].field_ordinal, 1);
+    xaot_bundle_free(&bundle);
+
+    xg_global_evidence_free(&ev);
+    teardown_parser_session();
+}
+
 TEST(global_evidence_producer_propagates_json_shape_through_field_initializer) {
     setup_parser_session();
     const char *source = "class Holder {\n"
@@ -14101,6 +14154,7 @@ RUN_TEST(global_evidence_producer_propagates_json_shape_through_ternary_assignme
 RUN_TEST(
     global_evidence_producer_does_not_propagate_json_shape_through_mismatched_ternary_assignment_return);
 RUN_TEST(global_evidence_producer_propagates_json_shape_through_closure_capture);
+RUN_TEST(global_evidence_producer_propagates_json_shape_through_array_closure_container);
 RUN_TEST(global_evidence_producer_propagates_json_shape_through_field_initializer);
 RUN_TEST(global_evidence_producer_propagates_json_shape_through_constructor_field_assignment);
 RUN_TEST(global_evidence_producer_propagates_json_shape_through_array_literal_container);
