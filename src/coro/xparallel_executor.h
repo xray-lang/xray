9c0587866ed58bce653c1eb25e7ae8490004dbbd
Xinglei Xu <xingleixu@gmail.com>
2026-07-12 04:51:05 +0800

    refactor(parallel): move aot batch join to runtime helper

--- src/coro/xparallel_executor.h
+++ src/coro/xparallel_executor.h
@@ -15,6 +15,7 @@
 #include <stdint.h>
 
 #include "../base/xconstants.h"
+#include "../os/os_thread.h"
 
 #ifndef XR_FUNC
 #define XR_FUNC extern
@@ -26,10 +27,21 @@ enum {
     XR_PARALLEL_EXECUTOR_MAX_LANES = XR_MAX_WORKERS,
 };
 
+typedef struct XrParallelJoin {
+    xr_mutex_t mutex;
+    xr_cond_t done;
+    int remaining_lanes;
+    bool initialized;
+} XrParallelJoin;
+
 XR_FUNC int xr_parallel_runtime_worker_cap(const struct XrRuntime *runtime, int max_lanes);
 XR_FUNC int xr_parallel_resolve_lane_count(const struct XrRuntime *runtime, int64_t item_count,
                                            int64_t requested_workers, int max_lanes);
 XR_FUNC bool xr_parallel_lane_bounds(int64_t start, int64_t end, int lane_count, int lane_id,
                                      int64_t *out_begin, int64_t *out_end);
+XR_FUNC void xr_parallel_join_init(XrParallelJoin *join, int remaining_lanes);
+XR_FUNC void xr_parallel_join_lane_done(XrParallelJoin *join);
+XR_FUNC void xr_parallel_join_wait(XrParallelJoin *join, struct XrRuntime *runtime);
+XR_FUNC void xr_parallel_join_destroy(XrParallelJoin *join);
 
 #endif /* XRAY_CORO_XPARALLEL_EXECUTOR_H */
